65d02e1a65cd5c208876987ff896c82dd17f7afd
aks47u <aks47u@hotmail.co.uk>
2014-05-02 12:02:40 +0100

    Create Q1_HackerRank_stdIn_stdOut_Practice.java

---
+++ MiNDS-Programathon-Practice-2014/Q1_HackerRank_stdIn_stdOut_Practice.java
@@ -0,0 +1,16 @@
+package MiNDS_Programathon_Practice_2014;
+
+import java.util.Scanner;
+
+public class Q1_HackerRank_stdIn_stdOut_Practice
+{
+	public static void main(String [] args)
+	{
+		Scanner scn = new Scanner(System.in);
+		int i = scn.nextInt();
+		for(int j = 0; j < i; j++)
+		{
+			System.out.println("Hello, world!");
+		}
+	}
+}
